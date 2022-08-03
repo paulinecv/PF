f3b82619c653308442924d7f040cd3f5cfa36f4e
samuel-gonz <110435856+samuel-gonz@users.noreply.github.com>
2022-08-03 19:55:21 -0400

    Update Borrador.R

--- Borrador.R
+++ Borrador.R
@@ -13,12 +13,31 @@ View(data_2016)
 head(data_2016)
 prueba
 
-#Comparar regiones entre si
+# Comparar regiones entre si
 
 tabla1 <- data_2016 %>% 
               group_by(Region) %>% 
               summarise(average = mean(Calification)) %>% 
               arrange(-average) %>% 
               ungroup()
-              
-y <- mean(data_2016$Calification)
+
+
+# Que variable tienen mayor ponderacion en el nivel de Felicidad
+
+mean_calification <- mean(data_2016$Calification)
+
+GDPporc <- mean(data_2016$`Economy (GDP per Capita)`) * 100 / mean_calification
+familyporc <- mean(data_2016$Family) * 100 / mean_calification
+healthporc <- mean(data_2016$`Health (Life Expectancy)`) * 100 / mean_calification
+freedomporc <- mean(data_2016$Freedom) * 100 / mean_calification
+corruptionporc <- mean(data_2016$`Trust (Government Corruption)`) * 100 / mean_calification
+generosityporc <- mean(data_2016$Generosity) * 100 / mean_calification
+dystopiaporc <- mean(data_2016$`Dystopia Residual`) * 100 / mean_calification
+
+tabla2 <- data.frame(Variables = c("GDP", "Family", "Health", "Freedom", "Corruption",
+                                 "Generosity", "Dystopia"),
+                     Porcentajes = c(GDPporc, familyporc, healthporc, freedomporc,
+                                     corruptionporc, generosityporc, dystopiaporc))
+
+ggplot(data = tabla2, aes(x = Variables, y = Porcentajes, fill = Variables))+
+  geom_bar(stat = "summary", fun = mean)
