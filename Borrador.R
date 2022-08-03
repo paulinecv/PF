4811a7966e6571d5bd354bd8def661f2d519ed45
samuel-gonz <110435856+samuel-gonz@users.noreply.github.com>
2022-08-03 15:41:41 -0400

    Create Borrador.R

---
+++ Borrador.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(readxl)
+library(readr)
+library(ggplot2)
+
+install.packages('cowplot')
+library(cowplot)
+
+setwd("C:/Users/Samuel/Desktop/Data.Science UCAB/ProyectoFinal")
+data_2016 <- read_csv("2016.csv")%>% 
+            rename( Calification = `Happiness Score`)
+View(data_2016)
+head(data_2016)
+
+
+#Comparar regiones entre si
+
+tabla1 <- data_2016 %>% 
+              group_by(Region) %>% 
+              summarise(average = mean(Calification)) %>% 
+              arrange(-average) %>% 
+              ungroup()
+              
+y <- mean(data_2016$Calification)
