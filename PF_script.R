306060c87c4ed33016b1794baaf7f2122965547a
camilograteol <camgraterol@gmail.com>
2022-08-04 18:56:16 -0400

    prueba de commit grafico_11

--- PF_script.R
+++ PF_script.R
@@ -153,3 +153,20 @@ grafico10 <- ggplot(data_2016, aes(PIB_per_capita, Calificacion)) +
             geom_point() +
             geom_smooth(method = "lm", colour = "Red")
 grafico10
+
+#Aqui se hace un grafico con multiples variables
+
+grafico_11<- data_2016 %>% ggplot() + 
+  geom_point(aes(Familia , Calificacion)) +
+  geom_smooth(aes(Familia,Calificacion),method=lm)+
+  xlab('Familia')
+
+grafico_11
+
+
+
+
+
+
+
+
